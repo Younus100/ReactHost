d54e2b2b9d170358b986c07eb3467448654a9c31
Younus <mohdyounussyed100@gmail.com>
2020-07-31 21:41:41 +0530

    Updates

---
+++ precache-manifest.0d67b159bdee29f07529c5443e437cb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c905d22f050380874bca409ac325bc4",
+    "url": "/ReactHost/index.html"
+  },
+  {
+    "revision": "c7ef37f2df5b5b4e4bc4",
+    "url": "/ReactHost/static/css/2.c7296f5a.chunk.css"
+  },
+  {
+    "revision": "ba17a0c724bcc325e289",
+    "url": "/ReactHost/static/css/main.06fbb0ee.chunk.css"
+  },
+  {
+    "revision": "c7ef37f2df5b5b4e4bc4",
+    "url": "/ReactHost/static/js/2.6376a236.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/ReactHost/static/js/2.6376a236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba17a0c724bcc325e289",
+    "url": "/ReactHost/static/js/main.2d898f7c.chunk.js"
+  },
+  {
+    "revision": "470e260f5a430a21ad2d",
+    "url": "/ReactHost/static/js/runtime-main.44a6b126.js"
+  }
+]);
